2367efc3aabde6f3f23e8c92dfa74c5392a6401a
Camille Bakkali <45128167+YiraSan@users.noreply.github.com>
2022-01-11 19:39:14 +0100

    add some logic and toStable

--- src/constructor/check.js
+++ src/constructor/check.js
@@ -1,16 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.typeCheck = void 0;
+exports.toStable = void 0;
 /**
  * Warning: This function use process.exit()
  */
-function typeCheck(packages) {
+function toStable(packages) {
     const keys = Object.keys(packages);
+    let stable = {};
+    // Turning packages to correct packages
     for (let i = 0; i < keys.length; i++) {
         const current = {
             key: keys[i],
             package: packages[keys[i]],
         };
+        stable[current.key] = {
+            threads: {},
+            class: {},
+            interface: {},
+            structure: {},
+        };
+        const threads = Object.keys(current.package.threads);
+        // threads
+        for (let t = 0; t < threads.length; t++) {
+            const thread = current.package.threads[threads[t]];
+            stable[current.key].threads[threads[t]] = {
+                fields: {},
+                methods: {},
+                flag: thread.flag,
+                static: thread.isStatic,
+            };
+            const fields = Object.keys(thread.fields);
+            // fields
+            for (let f = 0; f < fields.length; f++) {
+            }
+        }
     }
+    return stable;
 }
-exports.typeCheck = typeCheck;
+exports.toStable = toStable;
--- src/constructor/check.ts
+++ src/constructor/check.ts
@@ -1,11 +1,37 @@
-import { Packages } from "../converter/types";
+import { Method, Packages } from "../converter/types";
+
+export interface StableField {
+    value: boolean | string | number | null,
+}
+
+export interface StableThread {
+    fields: {[key: string]: StableField},
+    flag: "public" | "private",
+    static: boolean,
+    methods: {[key: string]: Method},
+}
+
+export interface StablePackage {
+    threads: {[key: string]: StableThread},
+    class: {},
+    interface: {},
+    structure: {},
+}
+
+export interface StablePackages  {
+    [key: string]: StablePackage
+}
 
 /**
  * Warning: This function use process.exit()
  */
-export function typeCheck (packages: Packages) {
+export function toStable (packages: Packages): StablePackages {
 
     const keys = Object.keys(packages);
+
+    let stable: StablePackages = {};
+
+    // Turning packages to correct packages
 
     for (let i = 0; i < keys.length; i++) {
 
@@ -14,8 +40,42 @@ export function typeCheck (packages: Packages) {
             package: packages[keys[i]],
         };
 
-        
+        stable[current.key] = {
+            threads: {},
+            class: {},
+            interface: {},
+            structure: {},
+        };
+
+        const threads = Object.keys(current.package.threads);
+
+        // threads
+        for (let t = 0; t < threads.length; t++) {
+
+            const thread = current.package.threads[threads[t]];
+
+            stable[current.key].threads[threads[t]] = {
+                fields: {},
+                methods: {},
+                flag: thread.flag,
+                static: thread.isStatic,
+            };
+
+            const fields = Object.keys(thread.fields);
+
+            // fields
+            for (let f = 0; f < fields.length; f++) {
+
+
+
+            }
+
+            
+
+        }
 
     }    
+
+    return stable;
 
 }
