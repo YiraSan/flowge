ba8187fa508af83292bd74e75f304dc4a3ea5c11
Camille Bakkali <45128167+YiraSan@users.noreply.github.com>
2022-01-11 22:07:51 +0100

    add type checking (base)

---
+++ src/builder/type.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.typeCheck = void 0;
+function typeCheck(context) {
+    let error = [];
+    return error;
+}
+exports.typeCheck = typeCheck;
---
+++ src/builder/type.ts
@@ -0,0 +1,16 @@
+import { RPAContext } from "../rpa/context";
+
+export interface TypeMsg {
+    message: string,
+    level: "error" | "warn",
+}
+
+export function typeCheck (context: RPAContext): TypeMsg[] {
+
+    let error: TypeMsg[] = [];
+
+    
+
+    return error;
+
+}
