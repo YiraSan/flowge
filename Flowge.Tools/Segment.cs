aea6c5c99e9d7b2e3689070853dd61c3c8fb6d2b
Camille Bakkali <45128167+YiraSan@users.noreply.github.com>
2022-01-26 20:59:25 +0100

    add segmentation from parenthesis

---
+++ Flowge.Tools/Segment.cs
@@ -0,0 +1,25 @@
+namespace Flowge.Tools
+{
+
+    public static class Segmenter
+    {
+
+        public static string[] From(string from)
+        {
+
+            List<string> to = new List<string>();
+
+            for (int i = 0; i < from.Length; i++)
+            {
+                
+                
+
+            }
+
+            return (string[]) to.ToArray();
+
+        }
+
+    }
+
+}
