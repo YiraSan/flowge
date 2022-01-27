264162851ae28928f9efaf1376a4d478f1d582a5
Camille Bakkali <45128167+YiraSan@users.noreply.github.com>
2022-01-27 15:10:23 +0100

    do some test

--- Flowge.Grammar/Parser.cs
+++ Flowge.Grammar/Parser.cs
@@ -1,3 +1,6 @@
+using Flowge.Tools;
+using Flowge.Lexing;
+
 namespace Flowge.Grammar 
 {
 
@@ -11,13 +14,97 @@ namespace Flowge.Grammar
             // todo
         }
 
+        private static List<Entry> Segment(string from)
+        {
+
+            from = from.Trim();
+
+            List<Entry> result = new List<Entry>();
+
+            string current = "";
+
+            if (from[0].Equals('(')) {
+                for (int i = 1; i < from.Length; i++)
+                {
+                    if (from[i].Equals(')')){
+                        result.Add(new Entry(current, false));
+                        result = (List<Entry>) result.Concat(Segment(String.Join("", from.Skip(i)))).ToList();
+                        break;
+                    }
+                    current += from[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < from.Length; i++)
+                {
+                    if (from[i].Equals(')'))
+                    {
+                        continue;
+                    }
+                    if (from[i].Equals('(')){
+                        result.Add(new Entry(current, true));
+                        result = (List<Entry>) result.Concat(Segment(String.Join("", from.Skip(i)))).ToList();
+                        break;
+                    }
+                    current += from[i];
+                    if (i == from.Length-1&&!current.Equals(""))
+                    {
+                        result.Add(new Entry(current, true));
+                    }
+                }
+            }
+
+            return result;
+
+        }
+
         private bool ParseLine(string[] exprs)
         {
 
             bool isGlobal = exprs[0].StartsWith("@");
             string name = exprs[0].Substring(1);
 
+            string[] expressions = String.Join("", exprs.Skip(1)).Split(",");
 
+            Lexer grammarLexer = new Lexer(new LexerEntry[]{
+
+                // keyword
+                new UntilEntry('\'', '\'', false),
+
+                // reference
+                (RegularEntry) "abcdefghijklmnopqrstuvwxyzABDEFGHIJKLMNOPQRSTUVWXYZ_",
+                (CharEntry) '.',
+                (CharEntry) '&',
+ 
+            });
+
+            // expression: pre build
+            for (int i = 0; i < expressions.Length; i++)
+            {
+
+                List<Entry> segmented = Segment(expressions[i]);
+
+                List<Section> sections = new List<Section>();
+
+                for (int e = 0; e < segmented.Count; e++)
+                {
+
+                    Section section = new Section(segmented[e].Required);
+
+                    grammarLexer.Set((string) segmented[e].Content);
+
+                    Token[] tokens = grammarLexer.Skip();
+
+                    Array.ForEach(tokens, c=>Console.WriteLine(c.getType()));
+
+                    sections.Add(section);
+
+                    Console.WriteLine();
+
+                }
+
+            }
 
             return true;
 
