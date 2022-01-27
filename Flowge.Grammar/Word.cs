1039d9c4c43d5bc3688a5c5e216f235ee39e6e05
Camille Bakkali <45128167+YiraSan@users.noreply.github.com>
2022-01-27 14:46:54 +0100

    add words rules

--- Flowge.Grammar/Word.cs
+++ Flowge.Grammar/Word.cs
@@ -13,6 +13,11 @@ namespace Flowge.Grammar
         public WordKind wordKind { get; }
         public string content { get; }
 
+        // rules
+
+        public bool PluralRule = false;
+        public bool DotRule = false;
+
         public Word(string content, WordKind wordKind)
         {
             this.content = content;
