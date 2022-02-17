7976321fee106f08a9c99788a2fefa477e8a2d23
Camille Bakkali <45128167+YiraSan@users.noreply.github.com>
2022-02-17 11:50:23 +0100

    add line support

--- src/terminal.js
+++ src/terminal.js
@@ -1,36 +1,67 @@
 const colors = require('colors');
 
+var ln = 0;
+
+function countLine(str){
+    if (typeof str !== "string") return 0;
+    return str.split("\n").length;
+}
+
 const term = {
 
     rtag: () => {
-      term.print("[ 🐲 ".gray + "Flowge".black + " ]".gray)
-      return term;
+        term.print("[ 🐲 ".gray + "Flowge".black + " ]".gray)
+        return term;
     },
   
     print: (str) => {
-      process.stdout.write(str+"".reset);
-      return term;
+        ln += countLine(str);
+        process.stdout.write(str+"".reset);
+        return term;
     },
   
     space: () => {
-      term.print(" ");
-      return term;
+        term.print(" ");
+        return term;
     },
   
     ln: () => {
-      term.print("\n");
-      return term;
+        term.print("\n");
+        return term;
     },
   
     println: (str) => {
-      term.print(str).ln();
-      return term;
+        term.print(str).ln();
+        return term;
     },
   
     tab: (str) => {
-      return "    "+str.split("\n").join("\n    ");
-    }
+        return "    "+str.split("\n").join("\n    ");
+    }, 
+
+    clear: (line) => {
+        const clearLastLine = () => {
+            process.stdout.moveCursor(0, -1); 
+            process.stdout.clearLine(1); 
+        }
+        if (typeof line !== "number") {
+            for (let i = 0; i < ln; i++) {
+                clearLastLine();
+                ln--;
+            }
+        } else {
+            for (let i = 0; i < line; i++) {
+                clearLastLine();
+                ln--;
+                if (ln == 0) {
+                    break;
+                }
+            }
+        }
+        process.stdout.moveCursor(0, -1);
+        return term;
+    },
   
-  }
+}
 
 module.exports = { term };
